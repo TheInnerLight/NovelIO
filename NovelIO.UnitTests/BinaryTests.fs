164ee067f200600e681a4f00332aa955c289f80c
Phil <phil@novelfs.org>
2015-12-03 22:50:56 +0000

    Binary IO basic unit tests
    Added tests of basic binary IO and fixed readFloat32 function as a
    result of testing

---
+++ NovelIO.UnitTests/BinaryTests.fs
@@ -0,0 +1,94 @@
+﻿namespace NovelFS.NovelIO.UnitTests
+
+open NovelFS.NovelIO
+open FsCheck
+open FsCheck.Xunit
+
+type ``Binary IO Tests`` =
+    [<Property>]
+    static member ``readByte from array of one byte`` (byte : byte) =
+        let bytes = [|byte|]
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readByte)) with
+            |IOSuccess (byte,_) -> byte
+            |IOError (_) -> failwith "error"
+        result = bytes.[0]
+    [<Property>]
+    static member ``readCharfrom array of one byte`` (character : char) =
+        let bytes = System.BitConverter.GetBytes(character)
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readByte)) with
+            |IOSuccess (byte,_) -> character
+            |IOError (_) -> failwith "error"
+        result = character
+    [<Property>]
+    static member ``readDecimal from array of one decimal`` (dec : decimal) =
+        let memoryStream = new System.IO.MemoryStream()
+        let binaryWriter = new System.IO.BinaryWriter(memoryStream);
+        binaryWriter.Write(dec)
+        let bytes = memoryStream.ToArray()
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readDecimal)) with
+            |IOSuccess (dec,_) -> dec
+            |IOError (_) -> failwith "error"
+        result = dec
+    [<Property>]
+    static member ``readFloat from array of one float`` (flt : float) =
+        let bytes = System.BitConverter.GetBytes flt
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readFloat)) with
+            |IOSuccess (flt,_) -> flt
+            |IOError (_) -> failwith "error"
+        match flt with
+        |_ when System.Double.IsNaN flt -> System.Double.IsNaN result
+        |_ -> result = flt
+    [<Property>]
+    static member ``readFloat32 from array of one float32`` (flt : float32) =
+        let bytes = System.BitConverter.GetBytes flt
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readFloat32)) with
+            |IOSuccess (flt,_) -> flt
+            |IOError (_) -> failwith "error"
+        match flt with
+        |_ when System.Single.IsNaN flt -> System.Single.IsNaN result
+        |_ -> result = flt
+    [<Property>]
+    static member ``readInt16 from array of one int16`` (int : int16) =
+        let bytes = System.BitConverter.GetBytes int
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readInt16)) with
+            |IOSuccess (int,_) -> int
+            |IOError (_) -> failwith "error"
+        result = int
+    [<Property>]
+    static member ``readInt32 from array of one int32`` (int : int32) =
+        let bytes = System.BitConverter.GetBytes int
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readInt32)) with
+            |IOSuccess (int,_) -> int
+            |IOError (_) -> failwith "error"
+        result = int
+    [<Property>]
+    static member ``readInt64 from array of one int64`` (int : int64) =
+        let bytes = System.BitConverter.GetBytes int
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readInt64)) with
+            |IOSuccess (int,_) -> int
+            |IOError (_) -> failwith "error"
+        result = int
+    [<Property>]
+    static member ``readString from array of one string`` (str : NonNull<string>) =
+        let str = str.Get
+        let memoryStream = new System.IO.MemoryStream()
+        let binaryWriter = new System.IO.BinaryWriter(memoryStream);
+        binaryWriter.Write(str)
+        let bytes = memoryStream.ToArray()
+        let result = 
+            match BinaryIO.run (MemoryBlockRead (bytes, BinaryIO.readString)) with
+            |IOSuccess (dec,_) -> str
+            |IOError (_) -> failwith "error"
+        result = str
+    
+        
+
+
---
+++ NovelIO.UnitTests/Scripts/load-references.fsx
@@ -0,0 +1,11 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r @"..\..\packages\FsCheck.2.2.3\lib\net45\FsCheck.dll"
+#r @"..\..\packages\FsCheck.Xunit.2.2.3\lib\net45\FsCheck.Xunit.dll"
+#r @"..\..\NovelIO\bin\Debug\NovelIO.dll"
+#r @"System.Core.dll"
+#r @"System.dll"
+#r @"System.Numerics.dll"
+#r @"..\..\packages\xunit.extensibility.core.2.1.0\lib\portable-net45+win8+wp8+wpa81\xunit.core.dll"
+#r @"..\..\packages\xunit.extensibility.execution.2.1.0\lib\net45\xunit.execution.desktop.dll"
+#r @"..\..\packages\xunit.runner.visualstudio.2.1.0\build\_common\xunit.abstractions.dll"
