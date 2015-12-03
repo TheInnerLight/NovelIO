ed765980de6aee59db5051a1115d6c6f5e9fe842
Phil <phil@novelfs.org>
2015-12-03 23:30:29 +0000

    Fake build script
    Added Fake build script

---
+++ Build.fsx
@@ -0,0 +1,34 @@
+﻿// include Fake lib
+#r @"packages/FAKE.4.10.3/tools/FakeLib.dll"
+open Fake
+
+let solution = "NovelIO.sln"
+let testDir  = "./test/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs ["bin"; "temp"])
+
+Target "Build" (fun _ ->
+    !! solution
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore)
+
+let testDlls = !! ("*Tests.dll")
+
+Target "xUnitTest" (fun _ ->
+    testDlls
+        |> Fake.Testing.XUnit.xUnit (fun p -> 
+            {p with 
+                ShadowCopy = false;}))
+
+Target "All" DoNothing
+
+// Build order
+"Clean"
+  ==> "Build"
+  ==> "xUnitTest"  // only if FAKE was called with parameter xUnitTest
+  ==> "All"
+
+// start build
+RunTargetOrDefault "All"
---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+"packages\FAKE.4.10.3\tools\Fake.exe" build.fsx
+pause
