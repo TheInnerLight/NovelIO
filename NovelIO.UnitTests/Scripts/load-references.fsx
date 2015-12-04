dda626113340d76c9a1b5fa010a476462e75f418
Phil <phil@novelfs.org>
2015-12-04 00:08:45 +0000

    Appveyor.yml

--- NovelIO.UnitTests/Scripts/load-references.fsx
+++ NovelIO.UnitTests/Scripts/load-references.fsx
@@ -2,10 +2,10 @@
 #I __SOURCE_DIRECTORY__
 #r @"..\..\packages\FsCheck.2.2.3\lib\net45\FsCheck.dll"
 #r @"..\..\packages\FsCheck.Xunit.2.2.3\lib\net45\FsCheck.Xunit.dll"
-#r @"..\..\NovelIO\bin\Debug\NovelIO.dll"
 #r @"System.Core.dll"
 #r @"System.dll"
 #r @"System.Numerics.dll"
 #r @"..\..\packages\xunit.extensibility.core.2.1.0\lib\portable-net45+win8+wp8+wpa81\xunit.core.dll"
 #r @"..\..\packages\xunit.extensibility.execution.2.1.0\lib\net45\xunit.execution.desktop.dll"
 #r @"..\..\packages\xunit.runner.visualstudio.2.1.0\build\_common\xunit.abstractions.dll"
+#r @"..\..\NovelIO\bin\Release\NovelIO.dll"
---
+++ build.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+"packages\FAKE\tools\FAKE.exe" "build.fsx"
