adc82550c5f6f9912a7c84ec91e020235a29b856
Phil <phil@novelfs.org>
2016-02-06 15:53:25 +0000

    Added first integration test and naming changes to unit tests

---
+++ NovelIO.IntegrationTests/IOTests.fs
@@ -0,0 +1,17 @@
+﻿namespace NovelFS.NovelIO.IntegrationTests
+
+open NovelFS.NovelIO
+open NovelFS.NovelIO.BinaryParser
+open FsCheck
+open FsCheck.Xunit
+
+type ``IO Integration Tests`` =
+    [<Property>]
+    static member ``Read All Bytes from file`` (bytes : byte[]) =
+        let fnameStr = "readbytestest.tst"
+        System.IO.File.WriteAllBytes(fnameStr, bytes)
+        let fname = File.assumeValidFilename fnameStr
+        match IO.run <| File.readAllBytes fname with
+        |IOSuccess readBytes ->
+            readBytes = bytes
+        |IOError err -> failwith "error"
