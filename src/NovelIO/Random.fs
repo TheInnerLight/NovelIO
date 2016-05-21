038f3b0c70f82f95f65863042701e38663f17526
Phil Curzon <phil@novelfs.org>
2016-05-21 20:38:09 +0100

    Updated copyright years and deleted commented out tests

--- src/NovelIO/Random.fs
+++ src/NovelIO/Random.fs
@@ -1,5 +1,5 @@
 ﻿(*
-   Copyright 2015 Philip Curzon
+   Copyright 2015-2016 Philip Curzon
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
